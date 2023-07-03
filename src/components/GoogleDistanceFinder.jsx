e31dffce1ee87a0fc5a339d61f5f1e405bb23b88
CodeRAFCE <seshan.io@prismscale.com>
2023-07-03 16:43:40 +0530

    getting the distance for pickup and drop

--- src/components/GoogleDistanceFinder.jsx
+++ src/components/GoogleDistanceFinder.jsx
@@ -7,12 +7,40 @@ import GoogleAutocomplete from "./forms/GoogleAutocomplete";
 const GoogleDistanceFinder = () => {
 	const [pickupAutocomplete, setPickupAutocomplete] = useState(null);
 	const [dropoffAutocomplete, setDropoffAutocomplete] = useState(null);
+	const [distance, setDistance] = useState(null);
+	console.log(distance);
 
 	const {
 		control,
 		formState: {errors},
 		setValue,
 	} = useFormContext();
+
+	const calculateDistance = () => {
+		if (pickupAutocomplete && dropoffAutocomplete) {
+			const origin = pickupAutocomplete.getPlace().geometry.location;
+			const destination = dropoffAutocomplete.getPlace().geometry.location;
+
+			const service = new window.google.maps.DistanceMatrixService();
+			service.getDistanceMatrix(
+				{
+					origins: [origin],
+					destinations: [destination],
+					travelMode: "DRIVING",
+				},
+				(response, status) => {
+					if (status === "OK") {
+						const result = response.rows[0].elements[0];
+						if (result.status === "OK") {
+							setDistance(result.distance.text);
+						}
+					} else {
+						console.log("Error calculating distance:", status);
+					}
+				}
+			);
+		}
+	};
 
 	const onLoadPickup = (autocomplete) => {
 		console.log("pickup autocomplete: ", autocomplete);
@@ -22,6 +50,7 @@ const GoogleDistanceFinder = () => {
 	const onLoadDropoff = (autocomplete) => {
 		console.log("dropoff autocomplete: ", autocomplete);
 		setDropoffAutocomplete(autocomplete);
+		// calculateDistance();
 	};
 
 	const onPlaceChangedForPickup = () => {
@@ -42,6 +71,7 @@ const GoogleDistanceFinder = () => {
 			console.log(place);
 			if (place) {
 				setValue("dropoff", place.formatted_address);
+				calculateDistance();
 			}
 		} else {
 			console.log("Dropoff Autocomplete is not loaded yet!");
@@ -50,6 +80,7 @@ const GoogleDistanceFinder = () => {
 
 	return (
 		<>
+			{distance && <p>Distance: {distance}</p>}
 			<div>
 				<Controller
 					name={"pickup"}
