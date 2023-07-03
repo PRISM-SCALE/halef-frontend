47fd82cabfd42e8aad83043107ccb67b862896f4
CodeRAFCE <seshan.io@prismscale.com>
2023-07-03 16:54:50 +0530

    removing unwanted comments

--- src/components/GoogleDistanceFinder.jsx
+++ src/components/GoogleDistanceFinder.jsx
@@ -17,32 +17,6 @@ const GoogleDistanceFinder = () => {
 		setValue,
 	} = useFormContext();
 
-	// const calculateDistance = () => {
-	// 	if (pickupAutocomplete && dropoffAutocomplete) {
-	// 		const origin = pickupAutocomplete.getPlace().geometry.location;
-	// 		const destination = dropoffAutocomplete.getPlace().geometry.location;
-
-	// 		const service = new window.google.maps.DistanceMatrixService();
-	// 		service.getDistanceMatrix(
-	// 			{
-	// 				origins: [origin],
-	// 				destinations: [destination],
-	// 				travelMode: "DRIVING",
-	// 			},
-	// 			(response, status) => {
-	// 				if (status === "OK") {
-	// 					const result = response.rows[0].elements[0];
-	// 					if (result.status === "OK") {
-	// 						setDistance(result.distance.text);
-	// 					}
-	// 				} else {
-	// 					console.log("Error calculating distance:", status);
-	// 				}
-	// 			}
-	// 		);
-	// 	}
-	// };
-
 	const onLoadPickup = (autocomplete) => {
 		console.log("pickup autocomplete: ", autocomplete);
 		setPickupAutocomplete(autocomplete);
