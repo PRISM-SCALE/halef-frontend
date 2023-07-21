548de514d84c54bdfef31df903a1c5c9369f4568
CodeRAFCE <seshan.io@prismscale.com>
2023-07-21 21:50:51 +0530

    Showing the response data in the UI

--- src/pages/ambulance/AirAmbulance.jsx
+++ src/pages/ambulance/AirAmbulance.jsx
@@ -11,6 +11,11 @@ import FormWrapper from "../../components/forms/FormWrapper";
 import Button from "../../components/forms/Button";
 import GoogleDistanceFinder from "../../components/forms/GoogleDistanceFinder";
 import {airAmbulanceCalculationService} from "../../utils/api";
+import CalculatorResultLayout from "../../components/CalculatorResultLayout";
+import {useResponsive} from "../../hooks/useResponsive";
+import CalculatorResultItem from "../../components/CalculatorResultItem";
+import {Box, Dialog, IconButton} from "@mui/material";
+import {Icon} from "@iconify-icon/react";
 
 const INITIAL_VALUES = {
 	region: "",
@@ -23,7 +28,19 @@ const INITIAL_VALUES = {
 };
 
 const AirAmbulance = () => {
+	const {mediumScreenAndUp} = useResponsive();
 	const [isChecked, setIsChecked] = useState(false);
+	const [airAmbulanceData, setAirAmbulanceData] = useState(null);
+
+	const [open, setOpen] = useState(false);
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
 
 	const methods = useForm({
 		defaultValues: {...INITIAL_VALUES},
@@ -47,8 +64,9 @@ const AirAmbulance = () => {
 
 	// ------------------------------------------------------
 	// * HANDLER FUNCTIONS
-	const onSubmit = (data) => {
-		airAmbulanceCalculationService(data);
+	const onSubmit = async (data) => {
+		const responseData = await airAmbulanceCalculationService(data);
+		setAirAmbulanceData(responseData);
 	};
 
 	const header_name = (
@@ -59,79 +77,122 @@ const AirAmbulance = () => {
 	return (
 		<ServiceWrapper>
 			<Header caption="cost estimation for" title={header_name} />
-			<FormProvider {...methods}>
-				<FormWrapper onSubmit={handleSubmit(onSubmit)}>
-					<div>
-						<label htmlFor="region" className="text-[#f8bf02]">
-							Select Your Region
-						</label>
-						<select
-							name="region"
-							className="input-fields appearance-none focus:outline-[#dd3333]"
-							{...register("region", {required: "Please select your region"})}
+			<div className="service-layout">
+				<div className="w-full">
+					<FormProvider {...methods}>
+						<FormWrapper onSubmit={handleSubmit(onSubmit)}>
+							<div>
+								<label htmlFor="region" className="text-[#f8bf02]">
+									Select Your Region
+								</label>
+								<select
+									name="region"
+									className="input-fields appearance-none focus:outline-[#dd3333]"
+									{...register("region", {required: "Please select your region"})}
+								>
+									<option value="">Choose your region</option>
+									<option value="domestic">Domestic</option>
+									<option value="international">International</option>
+								</select>
+								{errors.region && (
+									<p role="alert" className="text-[#ef4444] leading-none mt-1">
+										{errors.region?.message}
+									</p>
+								)}
+							</div>
+
+							<GoogleDistanceFinder
+							// originOptions={{types: ["airport"]}}
+							// destinationOptions={{types: ["airport"]}}
+							/>
+							{/* <CitiesFinder cities={cities} /> */}
+
+							<div className="w-full">
+								<label htmlFor="weight" className="text-[#f8bf02]">
+									Weight (kg)
+								</label>
+								<input
+									name="weight"
+									type="number"
+									className="input-fields focus:outline-[#dd3333] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+									placeholder="Enter your goods weight"
+									{...register("weight", {required: "Please enter the exact weight of your goods"})}
+									aria-invalid={errors.weight ? "true" : "false"}
+								/>
+								{errors.weight && (
+									<p role="alert" className="text-[#ef4444] leading-none mt-1">
+										{errors.weight?.message}
+									</p>
+								)}
+							</div>
+
+							{/* cost 5000/- */}
+							<div className="flex items-center gap-2">
+								<label htmlFor="isPackingRequired">Do you need packing?</label>
+								<input
+									type="checkbox"
+									name="isPackingRequired"
+									id="relocation-isPackingRequired"
+									{...register("isPackingRequired")}
+									checked={isChecked}
+									onChange={() => setIsChecked(!isChecked)}
+								/>
+							</div>
+
+							<Button
+								buttonText={
+									watch("region") === "international"
+										? "coming soon"
+										: watch("region") === "domestic"
+										? "calculate"
+										: "calculate"
+								}
+								disabled={watch("region") === "international"}
+								onClick={handleClickOpen}
+							/>
+						</FormWrapper>
+					</FormProvider>
+				</div>
+				{mediumScreenAndUp && airAmbulanceData ? (
+					<CalculatorResultLayout
+						imageName={"AIR AMBULANCE"}
+						// imageUrl={relocationData?.vehicleImage}
+					>
+						{airAmbulanceData?.airAmbulanceCost.map(({name, cost, unit}, index) => {
+							return <CalculatorResultItem key={index} title={name} value={cost} unit={unit} />;
+						})}
+					</CalculatorResultLayout>
+				) : (
+					<div className="text-center border bg-slate-50 border-slate-200 border-solid rounded-sm p-4 uppercase flex items-center justify-center">
+						<h1>Please enter the details to get calculated results</h1>
+					</div>
+				)}
+			</div>
+
+			{!mediumScreenAndUp && (
+				<Dialog fullScreen open={open} onClose={handleClose}>
+					<Box className="text-right mb-6">
+						<IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
+							<Icon icon="lucide:x" />
+						</IconButton>
+					</Box>
+
+					{airAmbulanceData ? (
+						<CalculatorResultLayout
+							imageName={airAmbulanceData?.vehicleName}
+							imageUrl={airAmbulanceData?.vehicleImage}
 						>
-							<option value="">Choose your region</option>
-							<option value="domestic">Domestic</option>
-							<option value="international">International</option>
-						</select>
-						{errors.region && (
-							<p role="alert" className="text-[#ef4444] leading-none mt-1">
-								{errors.region?.message}
-							</p>
-						)}
-					</div>
-
-					<GoogleDistanceFinder
-					// originOptions={{types: ["airport"]}}
-					// destinationOptions={{types: ["airport"]}}
-					/>
-					{/* <CitiesFinder cities={cities} /> */}
-
-					<div className="w-full">
-						<label htmlFor="weight" className="text-[#f8bf02]">
-							Weight (kg)
-						</label>
-						<input
-							name="weight"
-							type="number"
-							className="input-fields focus:outline-[#dd3333] [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-							placeholder="Enter your goods weight"
-							{...register("weight", {required: "Please enter the exact weight of your goods"})}
-							aria-invalid={errors.weight ? "true" : "false"}
-						/>
-						{errors.weight && (
-							<p role="alert" className="text-[#ef4444] leading-none mt-1">
-								{errors.weight?.message}
-							</p>
-						)}
-					</div>
-
-					{/* cost 5000/- */}
-					<div className="flex items-center gap-2">
-						<label htmlFor="isPackingRequired">Do you need packing?</label>
-						<input
-							type="checkbox"
-							name="isPackingRequired"
-							id="relocation-isPackingRequired"
-							{...register("isPackingRequired")}
-							checked={isChecked}
-							onChange={() => setIsChecked(!isChecked)}
-						/>
-					</div>
-
-					<Button
-						buttonText={
-							watch("region") === "international"
-								? "coming soon"
-								: watch("region") === "domestic"
-								? "calculate"
-								: "calculate"
-						}
-						disabled={watch("region") === "international"}
-						// onClick={handleClickOpen}
-					/>
-				</FormWrapper>
-			</FormProvider>
+							{airAmbulanceData?.relocationCost.map(({name, cost, unit}, index) => {
+								return <CalculatorResultItem key={index} title={name} value={cost} unit={unit} />;
+							})}
+						</CalculatorResultLayout>
+					) : (
+						<div className="text-center border bg-slate-50 border-slate-200 border-solid rounded-sm p-4 uppercase flex items-center justify-center">
+							<h1>Please enter the details to get calculated results</h1>
+						</div>
+					)}
+				</Dialog>
+			)}
 		</ServiceWrapper>
 	);
 };
