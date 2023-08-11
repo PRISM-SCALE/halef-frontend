49a13e3bccb4999aeb11a56bac11c2668128234f
Chandan Shankar <chandu.mechatronics@gmail.com>
2023-08-12 00:56:17 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,5 @@
 // export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3001/api";
-export const BASE_URL = import.meta.env.PROD
-	? "https://halef-calculator-api.onrender.com/api"
-	: "http://localhost:3001/api";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "http://localhost:3001/api";
 export const CALCULATE_URL = `${BASE_URL}/calculate`;
 
 export async function getAllServices() {
