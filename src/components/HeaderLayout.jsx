26665b6514466398ade38ddd3cbac619fbfbaa30
CodeRAFCE <seshan.io@prismscale.com>
2023-09-05 10:25:12 +0530

    adding links in social icons

--- src/components/HeaderLayout.jsx
+++ src/components/HeaderLayout.jsx
@@ -2,7 +2,7 @@ const HeaderLayout = () => {
 	return (
 		<div className="h-[45vh] bg-[url('https://halefinternational.com/wp-content/uploads/2023/07/stock-photo-low-angle-view-of-two-young-delivery-man-carrying-cardboard-box-in-front-of-truck-1486328387-transformed-transformed-copy-scaled-copy-1.webp')] bg-no-repeat bg-cover bg-right-top flex justify-center">
 			<div className="max-w-[540px] md:max-w-[720px] lg:max-w-[1300px] px-4 w-full mx-auto flex flex-col justify-center">
-				<div className="space-x-4 text-right space-y-2">
+				<div className="space-x-4 text-right space-y-4">
 					<a
 						href="https://halefinternational.com"
 						className="text-[#DD3333] hover:text-white transition-colors delay-300 ease-in-out text-lg"
@@ -13,7 +13,7 @@ const HeaderLayout = () => {
 					<span className="text-[#DD3333] text-lg">Cost estimator</span>
 				</div>
 				<div className="text-right">
-					<p className="md:text-7xl text-xl font-medium">
+					<p className="md:text-7xl text-xl font-medium leading-relaxed">
 						Cost <span className="text-[#DD3333]">Estimator</span>
 					</p>
 				</div>
--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -4,16 +4,47 @@ import PropTypes from "prop-types";
 const SocialIcons = ({size}) => {
 	return (
 		<div className="flex items-center gap-3">
-			<Icon icon="basil:facebook-outline" width={size} height={size} className="text-white" />
-			<Icon icon="basil:twitter-outline" width={size} height={size} className="text-white" />
-			<Icon icon="basil:instagram-outline" width={size} height={size} className="text-white" />
-			<Icon icon="eva:linkedin-outline" width={size} height={size} className="text-white" />
+			<Iconify
+				icon="basil:facebook-outline"
+				size={size}
+				link="https://www.facebook.com/Halefint2019?paipv=0&eav=AfZ8yvi6EeLUW89B0XI57QEbYjGvwOs-3hwrWCSz--Xs40oC_lXuL_rq-dauKAu-AzQ"
+			/>
+			<Iconify icon="basil:twitter-outline" size={size} link="https://twitter.com/halefint2019" />
+			<Iconify
+				icon="basil:instagram-outline"
+				size={size}
+				link="https://www.instagram.com/halefint2019/"
+			/>
+			<Iconify
+				icon="eva:linkedin-outline"
+				size={size}
+				link="https://www.linkedin.com/in/halef-international-600063276/"
+			/>
 		</div>
 	);
 };
 
 SocialIcons.propTypes = {
 	size: PropTypes.number.isRequired,
+};
+
+const Iconify = ({size, icon, link}) => {
+	return (
+		<a href={link} target="_blank" rel="noreferrer">
+			<Icon
+				icon={icon}
+				width={size}
+				height={size}
+				className="text-white cursor-pointer hover:text-[#DD3333] transition-colors ease-in-out delay-300"
+			/>
+		</a>
+	);
+};
+
+Iconify.propTypes = {
+	size: PropTypes.number.isRequired,
+	icon: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
 };
 
 export default SocialIcons;
