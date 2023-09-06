d75096c912d455a2d9e1b6890a94baca3bf9fff9
CodeRAFCE <seshan.io@prismscale.com>
2023-09-06 14:50:48 +0530

    fixing breadcrumb issue

--- src/components/HeaderLayout.jsx
+++ src/components/HeaderLayout.jsx
@@ -2,18 +2,18 @@ const HeaderLayout = () => {
 	return (
 		<div className="h-[40vh] md:h-[48vh] bg-no-repeat bg-cover bg-center md:bg-right-top flex justify-center bg-[url('https://halefinternational.com/wp-content/uploads/2023/07/stock-photo-low-angle-view-of-two-young-delivery-man-carrying-cardboard-box-in-front-of-truck-1486328387-transformed-transformed-copy-scaled-copy-1.webp')]">
 			<div className="max-w-[540px] md:max-w-[720px] lg:max-w-[1300px] px-4 w-full mx-auto flex flex-col justify-center">
-				<div className="space-x-4 text-right space-y-4">
+				<div className="space-x-4 text-right mb-2">
 					<a
 						href="https://halefinternational.com"
-						className="text-[#DD3333] hover:text-white transition-colors delay-300 ease-in-out text-lg"
+						className="text-[#DD3333] hover:text-white transition-colors delay-300 ease-in-out text-lg leading-5"
 					>
 						Home
 					</a>
 					<span className="text-[#DD3333]">/</span>
-					<span className="text-[#DD3333] text-lg">Cost estimator</span>
+					<span className="text-[#DD3333] text-lg leading-5">Cost estimator</span>
 				</div>
 				<div className="text-right">
-					<p className="md:text-7xl text-5xl font-medium leading-relaxed">
+					<p className="md:text-7xl text-5xl font-medium">
 						Cost <span className="text-[#DD3333]">Estimator</span>
 					</p>
 				</div>
